b38cea1fc1114640e44c859a8ba468ddec6a4ca2
zeno <giuseppezeno97@gmail.com>
2023-07-05 15:00:49 +0200

    esercizio 8

---
+++ Esercizi/esercizio-8/esercizio-8.mjs
@@ -0,0 +1,14 @@
+function luckyDraw(player) {
+    return new Promise((resolve, reject) => {
+      const win = Boolean(Math.round(Math.random()));
+  
+      process.nextTick(() => {
+        if (win) {
+          resolve(`${player} won a prize in the draw!`);
+        } else {
+          reject(new Error(`${player} lost the draw.`));
+        }
+      });
+    });
+  }
+
