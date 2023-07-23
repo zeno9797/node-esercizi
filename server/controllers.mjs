fe4681dab012970d089b17e5024120c9884b6417
zeno <giuseppezeno97@gmail.com>
2023-07-23 12:11:35 +0200

    aggiunto delete e update

--- server/controllers.mjs
+++ server/controllers.mjs
@@ -29,10 +29,30 @@ const getAll = async (req, res) => {
 
 const getOneById = async (req, res) => {
     const { id } = req.params;
-    const planet = await db.oneOrNone("SELECT * FROM planets")
-    res.status(200).json(planets)
+    const planet = await db.one("SELECT * FROM planets WHERE id=$1", id)
+    res.status(200).json(planet)
 }
 
-export {getAll, getOneById}
+const create = async (req, res) => {
+    const { name } = req.body;
+    await db.none(`INSERT INTO planets (name) VALUES ($1)`, name)
+    res.status(201).json({ msg: "the planet was created" })
+}
+
+const updateById = async (req, res) => {
+    const { id } = req.params
+    const { name } = req.body
+    await db.none(`UPDATE planets SET name=$2 WHERE id=$1`, [id, name])
+
+    res.status(200).json({ msg: "planet updated" })
+}
+
+const deleteById = async (req, res) => {
+    const { id } = req.params
+    await db.none(`DELETE FROM planets WHERE id=$1`, id)
+    res.status(200).json({ msg: "planet deleted" })
+}
+
+export {getAll, getOneById, create, deleteById, updateById}
 
 
--- server/index.mjs
+++ server/index.mjs
@@ -1,10 +1,20 @@
 import express from "express"
-import { getAll } from "./controllers.mjs";
+import { getAll, getOneById, create, deleteById, updateById } from "./controllers.mjs";
 
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/planets", getAll )
+
+app.get("/planets/:id", getOneById )
+
+app.post("/planets", create )
+
+app.delete("/planets/:id", deleteById)
+
+app.put("/planets/:id", updateById)
 
 app.listen(3000, () => {
     console.log("Server listening on port 3000");
